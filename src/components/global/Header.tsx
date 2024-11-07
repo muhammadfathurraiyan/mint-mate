686f6a3ce085f91292dcb50246bc1a8bf3e7faf7
muhammadfathurraiyan <raiyanfathur@gmail.com>
2024-11-07 13:35:44 +0700

    fix -> fix login bug

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,15 +1,5 @@
 "use client";
-import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetContent,
@@ -17,47 +7,36 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { generatePayload, isLoggedIn, login, logout } from "@/lib/auth";
 import { client } from "@/lib/client";
-import {
-  Hammer,
-  Home,
-  Image,
-  Menu,
-  Search,
-  Shapes,
-  Wallet,
-  X,
-} from "lucide-react";
+import { Hammer, Home, Image, Menu, Shapes, Wallet } from "lucide-react";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import { ConnectButton, darkTheme, lightTheme } from "thirdweb/react";
-import { Input } from "../ui/input";
 import { ThemeController } from "./ThemeController";
-import { useTheme } from "next-themes";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between lg:px-12 px-4 py-3">
-      <div className="flex items-center gap-6 mr-auto">
-        <Link href={"/"} className="font-bold text-2xl font-mono">
+      <div className="flex items-end gap-6 mr-auto">
+        <Link href={"/"} className="font-bold text-3xl font-mono">
           Mint<span className="font-light text-primary">Mate</span>
         </Link>
         <Link
           href={"/"}
-          className="text-muted-foreground hover:text-primary mt-1 text-sm transition-colors max-lg:hidden"
+          className="text-muted-foreground hover:text-primary mt-1 transition-colors max-lg:hidden"
         >
           Galery
         </Link>
         <Link
           href={"/"}
-          className="text-muted-foreground hover:text-primary mt-1 text-sm transition-colors max-lg:hidden"
+          className="text-muted-foreground hover:text-primary mt-1 transition-colors max-lg:hidden"
         >
           Mint
         </Link>
       </div>
       <div className="ml-auto flex items-center gap-2 max-lg:hidden">
         <ThemeController />
-        <AuthButton />
+        <LoginButton />
       </div>
       <div className="lg:hidden ml-auto">
         <Sidebar />
@@ -66,7 +45,7 @@ export default function Header() {
   );
 }
 
-function AuthButton() {
+function LoginButton() {
   const { theme } = useTheme();
   const LightTheme = lightTheme({
     colors: {
@@ -78,30 +57,23 @@ function AuthButton() {
     colors: {
       borderColor: "hsl(var(--border))",
       modalBg: "hsl(var(--card))",
+      
     },
   });
   return (
     <ConnectButton
-      connectButton={{ className: "connect-button" }}
+      connectButton={{ className: "connect-button", label: <ButtonLabel /> }}
       theme={theme === "light" ? LightTheme : DarkTheme}
       client={client}
-      auth={{
-        isLoggedIn: async (address: any) => {
-          console.log("checking if logged in!", { address });
-          return await isLoggedIn();
-        },
-        doLogin: async (params: any) => {
-          console.log("logging in!");
-          await login(params);
-        },
-        getLoginPayload: async ({ address }: { address: any }) =>
-          generatePayload({ address }),
-        doLogout: async () => {
-          console.log("logging out!");
-          await logout();
-        },
-      }}
     />
+  );
+}
+
+function ButtonLabel() {
+  return (
+    <>
+      <Wallet /> Connect
+    </>
   );
 }
 
@@ -116,7 +88,7 @@ function Sidebar() {
       <SheetContent>
         <SheetHeader>
           <SheetTitle className="text-left">
-            <Link href={"/"} className="font-bold text-2xl font-mono">
+            <Link href={"/"} className="font-bold text-3xl font-mono">
               Mint<span className="font-light text-primary">Mate</span>
             </Link>
           </SheetTitle>
@@ -152,7 +124,7 @@ function Sidebar() {
             <div>
               <ThemeController />
             </div>
-            <AuthButton />
+            <LoginButton />
           </div>
         </div>
       </SheetContent>
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,12 +1,8 @@
 import { createThirdwebClient } from "thirdweb";
 
-const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!;
-const secretKey = process.env.THIRDWEB_SECRET_KEY;
+const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!; // this will be used on the client
+const secretKey = process.env.THIRDWEB_SECRET_KEY!; // this will be used on the server-side
 
 export const client = createThirdwebClient(
-  secretKey
-    ? { secretKey }
-    : {
-        clientId,
-      }
+  secretKey ? { secretKey } : { clientId }
 );
