8698f4aa51b3f88a9276e1edb6f412b37c4f84c2
muhammadfathurraiyan <raiyanfathur@gmail.com>
2024-11-16 00:07:35 +0700

    fix -> fix build error

--- src/app/art/page.tsx
+++ src/app/art/page.tsx
@@ -6,22 +6,21 @@ import { useEffect } from "react";
 import { getNFTs } from "@/lib/action";
 
 export default function page() {
-  const context = seNFT();
+  const context = useNFT();
 
   if (!context) return "ups something went wrong!";
 
-  
   const fetchNFT = async () => {
     const nfts: TNft[] = await getNFTs();
     context.setData(nfts);
   };
-  
+
   useEffect(() => {
     if (context.data?.length === 0) fetchNFT();
   }, []);
-  
+
   if (context.data?.length === 0) return <Loading />;
-  
+
   return (
     <section className="lg:px-12 px-4 py-4 space-y-8">
       <div>
